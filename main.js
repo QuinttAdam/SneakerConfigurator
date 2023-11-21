011677f2d0d7dcf1beeca364ecd490437a669bd6
JarneSimons <r0901961@student.thomasmore.be>
2023-11-21 23:16:52 +0100

    feat: the sneaker is now visible on the screen

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-import './style.css'
-
+import './style.css';
 import * as THREE from 'three';
 
 // import orbit controls
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// import GLTF loader
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+//iport DRACOLoader
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+
+const draco = new DRACOLoader();
+draco.setDecoderConfig({ type: 'js' });
+draco.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.4.1/');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -13,14 +22,40 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
 
 
 // Orbit controls
 const controls = new OrbitControls( camera, renderer.domElement );
+
+
+// DRACOLoader
+const gltfLoader = new GLTFLoader();
+// const dracoLoader = new DRACOLoader();
+// dracoLoader.setDecoderPath('/examples/jsm/libs/draco/');
+// gltfLoader.setDRACOLoader(dracoLoader);
+gltfLoader.setDRACOLoader(draco);
+
+
+
+
+
+//load shoe model
+gltfLoader.load('/models/Shoe_compressed.glb', (gltf) => {
+  gltf.scene.scale.set(10, 10, 10);
+  gltf.scene.position.set(0, -1, 0);
+  scene.add(gltf.scene);
+
+  //add light to shoe
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(1, 1, 1).normalize();
+  scene.add(directionalLight);
+
+});
+
+
+
+
 
 
 camera.position.z = 5;
